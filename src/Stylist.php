4c4bdf260f5461aea9fcd678391c8a95937a49e8
Philip Putnam <putnamp989@gmail.com>
2017-02-24 12:08:29 -0800

    add test passing findStylistClients() to Stylist.php

--- src/Stylist.php
+++ src/Stylist.php
@@ -47,7 +47,17 @@
 
         static function findStylistsClients($stylist_id)
         {
-            
+            $returned_clients = $GLOBALS['DB']->query("SELECT * FROM clients WHERE stylist_id = {$stylist_id};");
+            $clients = array();
+            foreach($returned_clients as $client)
+            {
+                $name = $client['name'];
+                $stylist_id = $client['stylist_id'];
+                $id = $client['id'];
+                $new_client = new Client($name, $stylist_id, $id);
+                array_push($clients, $new_client);
+            }
+            return $clients;
         }
 
         static function getAll()
