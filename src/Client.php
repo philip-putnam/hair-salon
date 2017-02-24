3865e253b01f6db18e711c9ba9eec1abed09c4d9
Philip Putnam <putnamp989@gmail.com>
2017-02-24 10:44:07 -0800

    pass test_setName() in ClientTest.php

--- src/Client.php
+++ src/Client.php
@@ -17,9 +17,9 @@
             return $this->name;
         }
 
-        function setName()
+        function setName($new_name)
         {
-            
+            $this->name = $new_name;
         }
 
     }
