6a2656d85458003cae94b69bb6d994a276d17ddf
Philip Putnam <putnamp989@gmail.com>
2017-02-24 13:00:48 -0800

    add  variable to app.php, form to index.html.twig

--- app/app.php
+++ app/app.php
@@ -4,14 +4,19 @@
     require_once __DIR__.'/../src/Client.php';
     require_once __DIR__.'/../src/Stylist.php';
 
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
     $app = new Silex\Application();
     $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get('/', function() use($app) {
-
-        return $app['twig']->render("index.html.twig");
+        $stylists = Stylist::getAll();
+        return $app['twig']->render("index.html.twig", array('stylists' => $stylists));
     });
 
 
