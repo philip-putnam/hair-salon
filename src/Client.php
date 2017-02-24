3709377f38b2b159af12355028d802e7745af3cd
Philip Putnam <putnamp989@gmail.com>
2017-02-24 10:40:26 -0800

    add failing test_getName() to ClientTest.php, empty getName() in Client.php

---
+++ src/Client.php
@@ -0,0 +1,22 @@
+<?php
+    class Client
+    {
+        private $name;
+        private $stylist_id;
+        private $id;
+
+        function __construct($name, $stylist_id, $id = NULL)
+        {
+            $this->name = (string) $name;
+            $this->stylist_id = (int) $stylist_id;
+            $this->id = (int) $id;
+        }
+
+        function getName()
+        {
+            
+        }
+
+    }
+
+?>
---
+++ tests/ClientTest.php
@@ -0,0 +1,34 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once 'src/Client.php';
+    require_once 'src/Stylist.php';
+
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class ClientTest extends PHPUnit_Framework_TestCase
+    {
+        function test_getName()
+        {
+            //Arrange
+            $stylist = new Stylist("Debra Collins");
+            $stylist->save();
+
+            $client = new Client("George Clooney", $stylist->getId());
+
+            //Act
+            $result = "George Clooney";
+            $client_name = $client->getName();
+
+            //Assert
+            $this->assertEquals($client_name, $result);
+        }
+    }
+
+?>
