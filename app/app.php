147464fb4629c0e293c6d41fe53982b92d21d66c
Philip Putnam <putnamp989@gmail.com>
2017-02-24 13:25:36 -0800

    add /add-client/ route with add client to stylist functionality

--- app/app.php
+++ app/app.php
@@ -26,6 +26,20 @@
         return $app['twig']->render("index.html.twig", array('stylists' => $stylists));
     });
 
+    $app->get('/stylist/{id}', function($id) use($app) {
+        $clients = Stylist::findStylistsClients($id);
+        $stylist = Stylist::find($id);
+        return $app['twig']->render("stylist.html.twig", array('clients' => $clients, 'stylist' => $stylist));
+    });
+
+    $app->post('/add-client/{id}', function($id) use($app) {
+        $new_client = new Client($_POST['name'], $id);
+        $new_client->save();
+        $clients = Stylist::findStylistsClients($id);
+        $stylist = Stylist::find($id);
+        return $app['twig']->render("stylist.html.twig", array('clients' => $clients, 'stylist' => $stylist));
+    });
+
 
     return $app;
 ?>
